cc130d7d743cfbb5a09a94c174714a869e2d5697
Ahmed-Tameem <54918587+Ahmed-Tameem@users.noreply.github.com>
2019-11-06 20:40:01 -0800

    3

--- login.py
+++ login.py
@@ -1,38 +1,38 @@
-# return: username, size
-def get_info():
-    username = str(input("Enter your username: "))
-    user_size = len(username)
-    while user_size < 5:
-        print("Your username has to be at least 5 characters!")
-        username = str(input("Try another username: "))
-        user_size = len(username)
+class user:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-    password = str(input("Enter your password: "))
-    password_size = len(password)
-    while password_size < 5:
-        ("Your password has to be at least 5 characters!")
-        password = str(input("Try another password: "))
-        password_size = len(password)  
+    def get_info(self):
+        self.username = str(input("Enter your username: "))
+        user_size = len(self.username)
+        while user_size < 5:
+            print("Your username has to be at least 5 characters!")
+            self.username = str(input("Try another username: "))
+            user_size = len(self.username)
 
-    return username, password
+        self.password = str(input("Enter your password: "))
+        password_size = len(self.password)
+        while password_size < 5:
+            print("Your password has to be at least 5 characters!")
+            self.password = str(input("Try another password: "))
+            password_size = len(self.password)
 
-#return: 1 
-def write_info(username, password):
-    with open("data.txt", "a") as data_file:
-        data_file.write(username + "\t")
-        data_file.write(password + "\n")
-    return 1
-
-# class user:
-#     username
-#     password
+    def write_info(self):
+        with open("data.txt", "a") as data_file:
+            data_file.write(self.username + "\t")
+            data_file.write(self.password + "\n")
+            print("Welcome " + self.username + "!")
+            print("You are now successfully registerd!")
 
 
+while True:
+    my_user = user(None,None)
+    my_user.get_info()
+    my_user.write_info()
 
-username, password = get_info()
 
-if (write_info(username, password)):
-    print("Successfully registerd!")
-    print("Welcome " + username + "!")
-else:
-    print("Error registering your account")
+
+
+
+
