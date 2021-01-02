01f46175088c9b8fd297b90572d798d367a03654
Ahmed-Tameem <54918587+Ahmed-Tameem@users.noreply.github.com>
2021-01-02 14:17:26 +0300

    all docstrings done

--- Space_game/1.0/game.py
+++ Space_game/1.0/game.py
@@ -6,12 +6,11 @@
 #   to OOP and was lots of fun to make and play.
 """
 
-import turtle
-import random
-import math
-import winsound
+from turtle import Turtle, Screen
+from random import randint
+from winsound import Beep
 
-class Player(turtle.Turtle):
+class Player(Turtle):
     """Generates the player's avatar and handles the logic of it's movement.
 
     Attributes:
@@ -19,7 +18,13 @@ class Player(turtle.Turtle):
     """
 
     def __init__(self):
-        turtle.Turtle.__init__(self)
+        """Initializes the player's avatar.
+
+        Args: None.
+
+        Returns: None.
+        """
+        Turtle.__init__(self)
         self.penup()
         self.speed(0)
         self.color("yellow")
@@ -27,6 +32,13 @@ class Player(turtle.Turtle):
         self.velocity = 1
 
     def move(self):
+        """Controls the player's movement.
+
+        Args: None.
+
+        Returns: None.
+        """
+
         if self.xcor() > 320 or self.xcor() < -320 or self.ycor() > 320 or self.ycor() < -320:
             self.goto(0,0)
         if self.xcor() >= 280 or self.xcor() <= -280:
@@ -44,24 +56,60 @@ class Player(turtle.Turtle):
             self.forward(self.velocity)
 
     def turnleft(self):
+        """Conrtols the player's orientation.
+
+        Args: None.
+
+        Returns: None.
+        """
+
         self.left(30)
 
     def turnright(self):
+        """Conrtols the player's orientation.
+
+        Args: None.
+
+        Returns: None.
+        """
+
         self.right(30)
 
     def increasespeed(self):
+        """Conrtols the player's speed.
+
+        Args: None.
+
+        Returns: None.
+        """
+
         self.velocity += 1
 
     def decreasespeed(self):
+        """Conrtols the player's speed.
+
+        Args: None.
+
+        Returns: None.
+        """
+
         self.velocity -= 1
 
-class Border(turtle.Turtle):
+class Border(Turtle):
     """Generates the border of the game.
 
     Attributes: None.
     """
+
     def __init__(self):
-        turtle.Turtle.__init__(self)
+        """Initializes the border's attributes.
+
+        Args: None.
+
+        Returns: None.
+        """
+
+        Turtle.__init__(self)
         self.penup()
         self.hideturtle()
         self.speed(0)
@@ -69,6 +117,13 @@ class Border(turtle.Turtle):
         self.pensize(5)
 
     def draw_border(self):
+        """Draws the border.
+
+        Args: None.
+
+        Returns: None.
+        """
+
         self.penup()
         self.goto(-300,-300)
         self.pendown()
@@ -77,7 +132,7 @@ class Border(turtle.Turtle):
         self.goto(300,-300)
         self.goto(-300,-300)
 
-class Food(turtle.Turtle):
+class Food(Turtle):
     """Generates the food that the player eats for points.
 
     Attributes:
@@ -86,20 +141,41 @@ class Food(turtle.Turtle):
 
     amount = 4
     def __init__(self):
-        turtle.Turtle.__init__(self)
+        """Initializes the food's attributes.
+
+        Args: None.
+
+        Returns: None.
+        """
+
+        Turtle.__init__(self)
         self.penup()
         self.speed(0)
         self.color("green")
         self.shape("circle")
         self.velocity = 1.5
-        self.goto(random.randint(-250, 250), random.randint(-250, 250))
-        self.setheading(random.randint(0,360))
+        self.goto(randint(-250, 250), randint(-250, 250))
+        self.setheading(randint(0,360))
 
     def regenerate(self):
-        self.goto(random.randint(-250, 250), random.randint(-250, 250))
-        self.setheading(random.randint(0,360))
+        """Sets the initial location and heading of the food.
+
+        Args: None.
+
+        Returns: None.
+        """
+
+        self.goto(randint(-250, 250), randint(-250, 250))
+        self.setheading(randint(0,360))
 
     def move(self):
+        """Controls the movement of the food.
+
+        Args: None.
+
+        Returns: None.
+        """
+
         self.forward(self.velocity)
 
         if self.xcor() >= 280 or self.xcor() <= -280:
@@ -109,7 +185,7 @@ class Food(turtle.Turtle):
         if self.xcor() > 320 or self.xcor() < -320 or self.ycor() > 320 or self.ycor() < -320:
             self.regenerate()
 
-class Traps(turtle.Turtle):
+class Traps(Turtle):
     """Generates the traps that the player should avoid.
 
     Attributes:
@@ -118,20 +194,41 @@ class Traps(turtle.Turtle):
 
     amount = 2
     def __init__(self):
-        turtle.Turtle.__init__(self)
+        """Initializes the trap's attributes.
+
+        Args: None.
+
+        Returns: None.
+        """
+
+        Turtle.__init__(self)
         self.penup()
         self.shape("circle")
         self.color("red")
         self.speed(0)
         self.velocity = 0.5
-        self.goto(random.randint(-250, 250), random.randint(-250, 250))
-        self.setheading(random.randint(0,360))
+        self.goto(randint(-250, 250), randint(-250, 250))
+        self.setheading(randint(0,360))
 
     def regenerate(self):
-        self.goto(random.randint(-250, 250), random.randint(-250, 250))
-        self.setheading(random.randint(0,360))
+        """Sets the initial location and heading of the trap.
+
+        Args: None.
+
+        Returns: None.
+        """
+
+        self.goto(randint(-250, 250), randint(-250, 250))
+        self.setheading(randint(0,360))
 
     def move(self):
+        """Controls the movement of the trap.
+
+        Args: None.
+
+        Returns: None.
+        """
+
         self.forward(self.velocity)
 
         if self.xcor() >= 280 or self.xcor() <= -280:
@@ -141,7 +238,7 @@ class Traps(turtle.Turtle):
         if self.xcor() > 320 or self.xcor() < -320 or self.ycor() > 320 or self.ycor() < -320:
             self.regenerate()
 
-class Game(turtle.Turtle):
+class Game(Turtle):
     """Controls the score, collision detection, and ends the game when a trap is hit.
 
     Attributes:
@@ -149,7 +246,14 @@ class Game(turtle.Turtle):
     """
 
     def __init__(self):
-        turtle.Turtle.__init__(self)
+        """Initializes the game's attributes.
+
+        Args: None.
+
+        Returns: None.
+        """
+
+        Turtle.__init__(self)
         self.penup()
         self.hideturtle()
         self.speed(0)
@@ -158,50 +262,107 @@ class Game(turtle.Turtle):
         self.score = 0
 
     def update_score(self):
+        """Updates the score of the player.
+
+        Args: None.
+
+        Returns: None.
+        """
+
         self.clear()
         self.write("Score : {}".format(self.score), False, align = "left",
         font =("Arial", 14, "normal"))
 
     def change_score (self, points):
+        """Adds points to the player's score and updates it.
+
+        Args:
+            points (int): the amount by which the player's score is to be incremented.
+
+        Returns: None.
+        """
+
         self.score += points
         self.update_score()
 
     @staticmethod
     def touch_checker(the_player, the_food):
+        """Detects collisions between the player and the food.
+
+        Args:
+            the_player (Player): The object which has the attribute of the player's coordinates.
+            the_food (Food): The object which has the attribute of the food's coordinates.
+
+        Returns:
+            (bool): True if there is a collision, False otherwise.
+        """
+
         x_dis = the_player.xcor() - the_food.xcor()
         y_dis = the_player.ycor() - the_food.ycor()
 
-        distance = math.sqrt((x_dis ** 2) + (y_dis ** 2))
+        distance = ((x_dis ** 2) + (y_dis ** 2))**0.5
 
         return distance < 20
 
     def update_game_status(self):
+        """Displays the "Game Over" message.
+
+        Args: None.
+
+        Returns: None.
+        """
+
         self.goto(0,0)
         self.write("{}".format("Game Over"), False, align = "center", font =("Arial", 75, "normal"))
 
     @staticmethod
-    def game_over(the_player, the_traps):
-        x_dis = the_player.xcor() - the_traps.xcor()
-        y_dis = the_player.ycor() - the_traps.ycor()
+    def game_over(the_player, the_trap):
+        """Detects collisions between the player and traps.
 
-        distance = math.sqrt((x_dis ** 2) + (y_dis ** 2))
+        Args:
+            the_player (Player): The object which has the attribute of the player's coordinates.
+            the_trap (Trap): The object which has the attribute of the trap's coordinates.
+
+        Returns:
+            (bool): True if there is a collision, False otherwise.
+        """
+
+        x_dis = the_player.xcor() - the_trap.xcor()
+        y_dis = the_player.ycor() - the_trap.ycor()
+
+        distance = ((x_dis ** 2) + (y_dis ** 2))**0.5
 
         return distance < 20
 
-    def restart_game(self):
-        pass
-
     @staticmethod
     def play_eating_sound():
-        winsound.Beep(200, 10)
+        """Plays the eat sound.
+
+        Args: None.
+
+        Returns: None.
+        """
+
+        Beep(200, 10)
 
     @staticmethod
     def play_game_over_sound():
-        winsound.Beep(2000, 50)
+        """Plays the sound associated with the game ending.
+
+        Args: None.
+
+        Returns: None.
+        """
+        Beep(2000, 50)
 
 def main():
+    """Main loop where all the game logic is handled.
 
-    game_window = turtle.Screen()
+        Args: None.
+
+        Returns: None.
+    """
+    game_window = Screen()
     game_window.bgcolor("black")
     game_window.title("The Bumpy Boi Game")
     game_window.bgpic("background.gif")
